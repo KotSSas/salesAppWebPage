1d73d2c711c9cd1a53c4df443ec95066fb2941eb
Smug <sashasmug@gmail.com>
2022-06-10 22:35:47 +0200

    Initial commit

---
+++ src/main/java/com/example/salesappdemo/SalesAppDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.salesappdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalesAppDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SalesAppDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/salesappdemo/SalesAppDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.salesappdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalesAppDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
